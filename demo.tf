986a0114d05fe84abf403b843bd5cc25bdca9e69
vishal-savnani <vishalsav46@gmail.com>
2024-02-23 21:00:24 +0530

    module upload

---
+++ demo.tf
@@ -0,0 +1,32 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+      }
+    }
+}  
+
+provider "aws" {
+    profile = "default"
+  
+}
+
+data "aws_ami" "image" {
+    name_regex = "^al2023-ami-2023*"
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+      name = "name"
+      values = ["al2023-ami-2023*"]
+    }
+  
+}
+
+resource "aws_instance" "demo-instance" {
+    ami = data.aws_ami.image.id
+    instance_type = "t2.micro"
+
+
+}
